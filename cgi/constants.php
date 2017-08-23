dffb102bbd9e72a032a4c79efea95fe656376221
Ejz <ejz@ya.ru>
2017-08-23 18:04:24 +0300

    cgi / bootstrap

---
+++ cgi/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+define('RU_FORMAT_DATE', 'd.m.Y');
+define('RU_FORMAT_DATETIME', 'd.m.Y H:i:s');
+
+define('PAGINATOR_LIMIT', 10);
+define('DEFAULT_DEV_PASSWORD', 'default_dev_password');
+define('APPEND_TITLE', ' / Ejz');
+define('STAT_UNKNOWN', 'unknown');
